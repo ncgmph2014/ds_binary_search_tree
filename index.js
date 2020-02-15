fbe49261a5ef87eb221749dda9817c8a6ba373d0
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-15 16:43:58 -0500

    finished bst class

--- index.js
+++ index.js
@@ -17,6 +17,30 @@ class BST {
     if (node === null) {
       this.root = new Node(data);
       return;
+    } else {
+      const searchTree = function(node) {
+        if (data < node.data) {
+          //basically this means this should be the left side
+          if (node.left === null) {
+            //if there is nothing to the left, put something to the left
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            //i.e. if there is a node on the left, show me what it is.
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
     }
+    return searchTree(node);
   }
 }
